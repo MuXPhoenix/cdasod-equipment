0a7d972758755f98fc083b020b61753a4ee9c739
MuXPhoenix <3427870036@qq.com>
2018-11-23 10:12:45 +0800

    xf

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {LocalStorage} from "../shared/localStorage.service";
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: require('./menu.component.html'),
-  styleUrls: [require('./menu.component.css')]
+    selector: 'app-menu',
+    templateUrl: require('./menu.component.html'),
+    styleUrls: [require('./menu.component.css')]
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        private ls: LocalStorage,
+        private router : Router,) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    //退出登录
+    logout(){
+        this.ls.removeAll();
+        this.router.navigate(['/login']);
+    }
 
 }
